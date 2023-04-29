80bb3851953a02205d0e0dfa9c6dabe0185e920f
Piotr Michałowski <piotr5.12.13@gmail.com>
2023-04-29 13:14:30 +0200

    [FEATURE] EXTERNAL-API: add Json converter util

--- commons-lib/build.gradle.kts
+++ commons-lib/build.gradle.kts
@@ -1,3 +1,10 @@
+extra["jacksonVersion"] = "2.15.0"
+
+dependencies {
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${property("jacksonVersion")}")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${property("jacksonVersion")}")
+}
+
 tasks.getByName("bootJar") {
     enabled = false
 }
