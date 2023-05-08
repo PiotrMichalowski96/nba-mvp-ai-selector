e132a082ff5c139d9dbd707842fe48180d6067a4
Piotr Michałowski <piotr5.12.13@gmail.com>
2023-05-08 22:43:05 +0200

    [FEATURE] NBA-API-EVENTS: fix date time format

--- commons-lib/src/main/kotlin/pl/piter/commons/domain/MvpEvent.kt
+++ commons-lib/src/main/kotlin/pl/piter/commons/domain/MvpEvent.kt
@@ -1,5 +1,6 @@
 package pl.piter.commons.domain
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
 import java.time.OffsetDateTime
@@ -8,6 +9,7 @@ data class MvpEvent(
     @field:NotBlank val gameId: String,
     @field:NotBlank val homeTeam: String,
     @field:NotBlank val awayTeam: String,
+    @field:JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
     val startTime: OffsetDateTime,
     val gameResult: GameResult,
     @field:Valid val mvp: Player,
